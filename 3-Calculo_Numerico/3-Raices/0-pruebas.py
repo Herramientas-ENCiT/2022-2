cf51aa7d2db96b2695352fec79bb13cd46ab6667
Jonathan <jaurrutia.95@gmail.com>
2022-05-03 12:43:53 -0500

    Ejemplos y correccion

--- 3-Calculo_Numerico/3-Raices/0-pruebas.py
+++ 3-Calculo_Numerico/3-Raices/0-pruebas.py
@@ -6,6 +6,8 @@ import paq.raices as rt
 x = np.linspace(0,2*np.pi+.0001, 50)
 def func(x):
     return np.sin(2*x)-np.cos(x)*np.sin(3*x)
+def fprime(x):
+    return 2* np.sin(2*x) + np.sin(x)*np.sin(3*x) - 3*np.cos(x)*np.cos(3*x)
 
 
 plt.plot(x,func(x), marker = '*', label = r'$f(x)$')
@@ -16,10 +18,11 @@ raices = np.zeros(len(intervalos))
 print(intervalos)
 
 for i in range(0,len(intervalos)):
-    raices[i] = rt.bisection(func, intervalos[i], tol = 1e-8)
+    raices[i] = rt.newton_raphson(func, fprime, intervalos[i][0], tol = 1e-8, n_max = 100000)
 
-plt.plot(raices,np.zeros(len(raices)), marker = '*',label = 'raices')
+plt.plot(raices,np.zeros(len(raices)),  '*',label = 'raices')
 print(raices)
 
+plt.xlim([0,2*np.pi])
 plt.legend()
 plt.show()
--- 3-Calculo_Numerico/3-Raices/paq/raices.py
+++ 3-Calculo_Numerico/3-Raices/paq/raices.py
@@ -18,7 +18,7 @@ def root_int(f, x_list):
 
     return np.array(intervalos)
 
-def bisection(f, int, tol = .00001, n_max = 1000):
+def bisection(f, int, tol = 1e-7, n_max = 1000, iteration_num = False):
     '''
     Metodo de biseccion para calcilar raices en un intervalo donde se
     garantice que hay una.
@@ -31,7 +31,7 @@ def bisection(f, int, tol = .00001, n_max = 1000):
     delta_x = x_right - x_left
     mid_point = x_left + 0.5 * delta_x
 
-    while (delta_x/2 > tol and count < n_max):
+    while (delta_x / 2 > tol and count < n_max):
         if f(x_left) * f(mid_point) > 0:
             x_left = mid_point
         elif f(mid_point) * f(x_right) > 0:
@@ -41,5 +41,53 @@ def bisection(f, int, tol = .00001, n_max = 1000):
         count += 1
         delta_x = x_right - x_left
         mid_point = x_left + 0.5 * delta_x
+    if interation_num:
+        return [mid_point, count]
+    else:
+        return mid_point
 
-    return mid_point
+def secant(f, int, tol = 1e-7, n_max = 1000, iteration_num = False):
+    '''
+    Metodo de sección para calcilar raices en un intervalo donde se
+    garantice que hay una.
+    f es una función
+    int es una lista de dos elementos donde int[0]<int[1]
+    '''
+    count = 0
+    x_0 = int[0]
+    x_1 = int[1]
+    rts = x_0
+    delta_x = -f(x_0) * (x_1 - x_0) / (f(x_1) - f(x_0))
+
+    while (delta_x / 2 > tol and count < n_max):
+        rts += delta_x
+        x_0 = x_1
+        x_1 = rts
+
+        delta_x = -f(x_0) * (x_1 - x_0) / (f(x_1) - f(x_0))
+        count += 1
+    if iteration_num:
+        return [rts, count]
+    else:
+        return rts
+
+def newton_raphson(f, fprime, x_0, tol = 1e-7, n_max = 1000, iteration_num = False):
+    '''
+    Metodo de Newton-Raphson para el cálculo de raices. Es necesario definir:
+    f es una función
+    fprime es la derivada de la función a e entonctrar sus raices
+    x_0 es un punto a evaluar la función
+    '''
+    x_i = x_0
+    delta_x = -f(x_i) / fprime(x_i)
+    x_i += delta_x
+    count = 1
+
+    while ( abs(delta_x) / 2 > tol and count < n_max):
+        delta_x = -f(x_i) / fprime(x_i)
+        x_i += delta_x
+        count += 1
+    if iteration_num:
+        return [x_i, count]
+    else:
+        return x_i
